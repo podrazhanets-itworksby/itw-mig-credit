c8dba4dad43d97c8375870664a561367b789190a
Nikolay Podrazhanets <n.podrazhanets@apibank.ru>
2022-03-01 07:51:51 +0300

    init

--- mig-credit/src/main/java/by/itworks/migcredit/controller/WsController.java
+++ mig-credit/src/main/java/by/itworks/migcredit/controller/WsController.java
@@ -2,6 +2,8 @@ package by.itworks.migcredit.controller;
 
 import by.itworks.migcredit.domain.NewData;
 import by.itworks.migcredit.service.ClientService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -16,8 +18,11 @@ public class WsController {
 	private final SimpMessagingTemplate simpMessagingTemplate;
 
 	@MessageMapping({"/new-data"})
-	public void sendNewDataToClient(@Payload NewData data) {
+	public void sendNewDataToClient(@Payload String data) throws JsonProcessingException {
+		data = data.substring(1, data.length() - 1).replaceAll("\\\\", "");
 		System.out.println(data);
+		ObjectMapper mapper = new ObjectMapper();
+		NewData newData = mapper.readValue(data, NewData.class);
 		simpMessagingTemplate.convertAndSend("/topic/send/", data);
 	}
 }
--- mig-credit/src/main/java/by/itworks/migcredit/domain/NewData.java
+++ mig-credit/src/main/java/by/itworks/migcredit/domain/NewData.java
@@ -1,53 +1,13 @@
 package by.itworks.migcredit.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 public class NewData {
-	private String sessionId;
-	private String fieldName;
-	private String fieldValue;
-
-	public NewData(){}
-
-	public NewData(String sessionId, String fieldName, String fieldValue) {
-		this.sessionId = sessionId;
-		this.fieldName = fieldName;
-		this.fieldValue = fieldValue;
-	}
-
-	public String getSessionId() {
-		return sessionId;
-	}
-
-	public void setSessionId(String sessionId) {
-		this.sessionId = sessionId;
-	}
-
-	public String getFieldName() {
-		return fieldName;
-	}
-
-	public void setFieldName(String fieldName) {
-		this.fieldName = fieldName;
-	}
-
-	public String getFieldValue() {
-		return fieldValue;
-	}
-
-	public void setFieldValue(String fieldValue) {
-		this.fieldValue = fieldValue;
-	}
-
-	@Override
-	public String toString() {
-		return "NewData{" +
-				"sessionId='" + sessionId + '\'' +
-				", fieldName='" + fieldName + '\'' +
-				", fieldValue='" + fieldValue + '\'' +
-				'}';
-	}
+	public String sessionId;
+	public String fieldName;
+	public String fieldValue;
 }
